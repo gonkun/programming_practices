38eebc2ac5c35e0fdf58d49fedd2b96b594609fd
Gon <gonzalo.sanchter@gmail.com>
2018-09-23 23:56:25 +0200

    add exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import datetime
+
+print("Enter your name: ")
+name = input("> ")
+
+print("Enter your age: ")
+age = int(input("> "))
+born_year = int(datetime.date.today().year) - age
+
+print("How many time do you want toprint the same message? ")
+times = int(input("> "))
+
+print(times * f"Your name is {name} and you will be 100 years on {born_year + 100}\n")
