c471d05cee61db0878fc3022e5ab88e9c34b2548
Gonzalo Sanchez <gonzalo.sanchter@gmail.com>
2020-04-16 21:25:06 +0200

    Update printTable.py

--- automate_boring_stuff/printTable.py
+++ automate_boring_stuff/printTable.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 # printTable.py - Print a Table with Strings right-justified
+
+tableData = [
+    ["apples", "oranges", "cherries", "banana"],
+    ["Alice", "Bob", "Carol", "David"],
+    ["dogs", "cats", "moose", "goose"],
+]
+
+
+def printTable(table):
+    colWidth = []
+    for list in table:
+        colWidth.append(len(max(list, key=len)))
+    print(colWidth)
+
+
+printTable(tableData)
