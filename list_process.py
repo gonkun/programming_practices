09a1a5383c2a5c0d7c15dc49045b386cc6ab5e3e
Gonzalo Sanchez <gsanchez@userzoom.com>
2020-05-06 00:29:15 +0200

    new code

--- list_process.py
+++ list_process.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import psutil
+#import tabulate
+import netifaces
+import re
+import pprint
 
 
 def findProcessIdByName(process_name):
@@ -25,10 +29,34 @@ def findProcessIdByName(process_name):
 
 
 def getParametersProcessByPid(list_pid):
+    list_of_parameters = []
     for pid in list_pid:
         p = psutil.Process(pid)
-        print(p.cmdline())
+        list_of_parameters.append(p.cmdline()[2])
+    # print(list_of_parameters)
+    return list_of_parameters
 
 
-openvpn_pids = findProcessIdByName("openvpn")
-getParametersProcessByPid(openvpn_pids)
+def getVPNAddresses():
+    vpn_networks = {}
+    r = re.compile(r'tun\d')
+    interfaces = netifaces.interfaces()
+    vpn_interfaces = list(filter(r.match, interfaces))
+
+    if not vpn_interfaces:
+        return vpn_networks
+    else:
+        for vpn in vpn_interfaces:
+            addrs = netifaces.ifaddresses(vpn)
+            vpn_addr = addrs[netifaces.AF_INET][0]['addr']
+            vpn_networks[vpn] = vpn_addr
+        return vpn_networks
+
+
+# openvpn_pids = findProcessIdByName("openvpn")
+# list_openvpn_parameters = getParametersProcessByPid(openvpn_pids)
+
+
+dict_vpn = getVPNAddresses()
+pprint.pprint(dict_vpn)
+# print(tabulate(list_openvpn_parameters))
