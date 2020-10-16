3b471d8d237b9fe26020988833feefe6b6e12c80
Gonzalo Sanchez <gsanchez@userzoom.com>
2020-10-16 09:05:13 +0200

    rewrite examples

--- python_for_devops/find_rc.py
+++ python_for_devops/find_rc.py
@@ -1,36 +1,37 @@
 import os
+import pathlib
 
 
 def find_rc(rc_name=".examplerc"):
 
     # Check for Env variable
     var_name = "EXAMPLERC_DIR"
-    if var_name in os.environ:
-        var_path = os.path.join(f"${var_name}", rc_name)
-        config_path = os.path.expandvars(var_path)
+    example_dir = os.environ.get(var_name)
+    if example_dir:
+        dir_path = pathlib.Path(example_dir)
+        config_path = dir_path / rc_name
         print(f"Checking {config_path}")
-        if os.path.exists(config_path):
-            return config_path
+        if config_path.exists():
+            return config_path.as_postfix()
 
     # Check the current working directory
-    config_path = os.path.join(os.getcwd(), rc_name)
+    config_path = pathlib.Path.cwd() / rc_name
     print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    if config_path.exists():
+        return config_path.as_postfix()
 
     # Check user home directory
-    home_dir = os.path.expanduser("~/")
-    config_path = os.path.join(home_dir, rc_name)
+    config_path = pathlib.Path.home() / rc_name
     print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    if config_path.exists():
+        return config_path.as_postfix()
 
     # Check Directory of This File
-    file_path = os.path.abspath(__file__)
-    parent_path = os.path.dirname(file_path)
-    config_path = os.path.join(parent_path, rc_name)
-    print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    file_path = pathlib.Path(__file__).resolve()
+    parent_path = file_path.parent
+    config_path = parent_path / rc_name
+    print(f"Checking {parent_path}")
+    if config_path.exists():
+        return config_path.as_postfix()
 
     print(f"File {rc_name} has not been found")
--- python_for_devops/os_path_walk.py
+++ python_for_devops/os_path_walk.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import fire
 import os
 
 
 def walk_path(parent_path):
-    print(f"Checking: {parent_path}")
-    childs = os.listdir(parent_path)
-
-    for child in childs:
-        child_path = os.path.join(parent_path, child)
-        if os.path.isfile(child_path):
-            last_access = os.path.getatime(child_path)
-            size = os.path.getsize(child_path)
-            print(f"File: {child_path}")
+    for parent_path, directories, files in os.walk(parent_path):
+        print(f"Checking: {parent_path}")
+        for file_name in files:
+            file_path = os.path.join(parent_path, file_name)
+            last_access = os.path.getatime(file_path)
+            size = os.path.getsize(file_path)
+            print(f"File: {file_path}")
             print(f"\tlast accessed: {last_access}")
             print(f"\tsize: {size}")
-        elif os.path.isdir(child_path):
-            walk_path(child_path)
 
 
 if __name__ == '__main__':
