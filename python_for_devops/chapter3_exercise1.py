10d1d88a799ebb0fec7790b05a23ea5cf609c369
Gonzalo Sanchez <gsanchez@userzoom.com>
2020-11-17 07:45:58 +0100

    add more exercises

---
+++ python_for_devops/chapter3_exercise1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+    Simple command-line tool that prints the command line
+"""
+import sys
+
+if __name__ == '__main__':
+    print(f"The command line is: '{sys.argv[0]}'")
