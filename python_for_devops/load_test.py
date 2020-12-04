24c22606736872b88e12a38567de7a08886712bf
Gonzalo Sanchez <gsanchez@userzoom.com>
2020-12-04 09:25:37 +0100

    add load_test.py

--- python_for_devops/load_test.py
+++ python_for_devops/load_test.py
@@ -6,7 +6,15 @@ Test molotov Load Testing tool
 import molotov
 
 
-@molotov.scenario(100)
+@molotov.scenario()
 async def scenario_one(session):
-    async with session.get("http://localhost:5000") as resp:
+    async with session.get("http://localhost:5000/") as resp:
         assert resp.status == 200
+
+
+@molotov.scenario(80)
+async def scenario_two(session):
+    resp = await session.post("http://localhost:5000", params={'q': 'devops'})
+    redirect_status = resp.history[0].status
+    error = "unexpected redirect status: %s" % redirect_status
+    assert redirect_status == 301, error
